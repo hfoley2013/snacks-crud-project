1e5d8dfd6020577993ea038100eeee8e241ba494
Harper Foley <harper.e.foley@gmail.com>
2023-01-25 16:56:07 -0800

    first commit

---
+++ snacks/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class Snack(models.Model):
+    name = models.CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.TextField(max_length=256)
+    image_url = models.URLField(default='https://upload.wikimedia.org/wikipedia/commons/c/ce/7-11_selling_all_kinds_of_snacks_in_Taiwan.jpg')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('snack_detail', args=[str(self.id)])
